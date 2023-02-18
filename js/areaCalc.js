e45afa5474df93dee18454d35019a25deab2785b
MafujulHaquePlabon <mafujul15-3076@diu.edu.bd>
2023-02-18 10:28:40 +0600

    Rectangle parallelogram area calculation

--- js/areaCalc.js
+++ js/areaCalc.js
@@ -97,3 +97,49 @@ document
       "area-Parent"
     );
   });
+  /*=================Rectangle,Parallelogram function==================== */
+  function getAreaRectangleParallelogram(cardFirstInput, card2ndInput, cardtitle, areaparent) {
+    const cardInput1Field = document.getElementById(cardFirstInput);
+    const cardInput1Value = parseFloat(cardInput1Field.value);
+    const cardInput2Field = document.getElementById(card2ndInput);
+    const cardInput2Value = parseFloat(cardInput2Field.value);
+    const area =cardInput1Value * cardInput2Value;
+    const cardTitle = document.getElementById(cardtitle);
+    const cardTitleInnerText = cardTitle.innerText;
+    const areaParent = document.getElementById(areaparent);
+    const newElement = document.createElement("li");
+    newElement.setAttribute("class", "py-2");
+    newElement.innerHTML = `
+      <div>
+      <tr>
+      <td>${cardTitleInnerText}</td>
+      <td>${area}<span>cm<sup>2</sup></span></td>
+      <td> <button class="btn blog text-white fw-bold py-1 px-2" type="submit">Convert to m <sup>2</sup></button></td>
+    </tr>
+    <tr>
+      </div>`;
+    areaParent.appendChild(newElement);
+    cardInput1Field.value='';
+    cardInput2Field.value='';
+  }
+  document
+  .getElementById("card2-Calc-Btn")
+  .addEventListener("click", function () {
+    getAreaRectangleParallelogram(
+      "card2-W-Input",
+      "card2-I-Input",
+      "card-title-rectangle",
+      "area-Parent"
+    );
+  });
+  document
+  .getElementById("card3-Calc-Btn")
+  .addEventListener("click", function () {
+    getAreaRectangleParallelogram(
+      "card2-B-Input",
+      "card2-H-Input",
+      "card-title-parallelogram",
+      "area-Parent"
+    );
+  });
+  
